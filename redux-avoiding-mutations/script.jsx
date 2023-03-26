d5af243ff8a92cd63592874d601e004f725e4d09
Shivam Kadwade <83583221+shivam-kadwade@users.noreply.github.com>
2023-03-27 00:46:33 +0530

    Added simple counter increment function

---
+++ redux-avoiding-mutations/script.jsx
@@ -0,0 +1,14 @@
+
+const addCounter = (list) =>{
+    list.push(0)
+    return list
+}
+
+const testAddCounter = ()=>{
+    const listBefore = []
+    const listAfter = [0]
+
+    expect(addCounter(listBefore)).toEqual(listAfter)
+}
+testAddCounter()
+console.log("All tests passed!")
