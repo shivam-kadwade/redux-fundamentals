b83e5a5ec9aba29bdd8ff92cbcb25de927f0e36a
Shivam Kadwade <83583221+shivam-kadwade@users.noreply.github.com>
2023-03-28 15:33:35 +0530

    Added removeCounter which mutates redux state

--- redux-avoiding-mutations/script.jsx
+++ redux-avoiding-mutations/script.jsx
@@ -4,6 +4,12 @@ const addCounter = (list) =>{
     return list
 }
 
+const removeCounter = (list, index) =>{
+    list.splice(index,1)
+    return list
+}
+
+
 const testAddCounter = ()=>{
     const listBefore = []
     const listAfter = [0]
@@ -11,4 +17,12 @@ const testAddCounter = ()=>{
     expect(addCounter(listBefore)).toEqual(listAfter)
 }
 testAddCounter()
+
+const testRemoveCounter = ()=>{
+    const listBefore = [1,10,11]
+    const listAfter = [1,11]
+
+    expect(removeCounter(listBefore,1)).toEqual(listAfter)
+}
+testRemoveCounter()
 console.log("All tests passed!")
