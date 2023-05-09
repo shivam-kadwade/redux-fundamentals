449fc62493c5ae26f204129689c9868ee980a526
Shivam Kadwade <83583221+shivam-kadwade@users.noreply.github.com>
2023-05-09 18:10:28 +0530

    Added testincrement counter

--- redux-avoiding-mutations/script.jsx
+++ redux-avoiding-mutations/script.jsx
@@ -8,6 +8,9 @@ const removeCounter = (list, index) =>{
     return [...list.slice(0,index),...list.slice(index+1)]
 }
 
+const incrementCounter = (list, index)=>{
+return [...list.slice(0,index), list[index] +1, ...list.splice(index+1)]
+}
 
 const testAddCounter = ()=>{
     const listBefore = []
@@ -24,4 +27,13 @@ const testRemoveCounter = ()=>{
     expect(removeCounter(listBefore,1)).toEqual(listAfter)
 }
 testRemoveCounter()
+
+const testIncrementCounter = ()=>{
+    const listBefore = [1,10,22]
+    const listAfter = [1,11,22]
+
+    expect(incrementCounter(listBefore,1)).toEqual(listAfter)
+}
+testIncrementCounter()
+
 console.log("All tests passed!")
